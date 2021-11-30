db4c58ddbe925789f18236c9c3f5c35a11a7c99d
Jie Pan <hanamichi401@hotmail.com>
2021-11-30 12:15:02 -0500

    add api project

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+namespace BookStoreApi.Models
+{
+    public enum BookStatus
+    {
+        InStore,
+        Borrowed,
+        Lost
+    }
+    public class Book
+    {
+
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string? Description { get; set; }
+        public string CoverName { get; set; }
+        public BookStatus Status { get; set; }
+    }
+
+}
---
+++ Models/BookStoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Models
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books {get; set; } = null!;
+    }
+}
