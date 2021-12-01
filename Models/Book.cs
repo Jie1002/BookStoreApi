57a98f54be5181884e45f0bb3d3dc72f564eee3f
Jie Pan <hanamichi401@hotmail.com>
2021-12-01 16:38:09 -0500

    update

--- Models/Book.cs
+++ Models/Book.cs
@@ -14,7 +14,7 @@ namespace BookStoreApi.Models
         public string Author { get; set; }
         public string? Description { get; set; }
         public string CoverName { get; set; }
-        public BookStatus Status { get; set; }
+        public int Status { get; set; }
     }
 
 }
